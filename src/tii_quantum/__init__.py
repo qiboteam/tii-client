37581075ec9dd77b667261262c9d9248b0c99dd9
Stefano Carrazza <stefano.carrazza@gmail.com>
2026-02-03 13:49:41 +0100

    fixing pre-commit

--- src/tii_quantum/__init__.py
+++ src/tii_quantum/__init__.py
@@ -4,5 +4,6 @@ import importlib.metadata as im
 
 __version__ = im.version(__package__)
 
-from .tii_client import Client
 from qibo_client.qibo_job import QiboJob, QiboJobStatus
+
+from .tii_client import Client
